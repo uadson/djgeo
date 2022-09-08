3a95bf137bfa51ee451b30e0a5833769540f65c6
Uadson Feitosa <uadsonpy@gmail.com>
2022-09-08 17:19:45 -0300

    created function index and your url in views.py and urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from core.views import get_index
+from core.views import index
 
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', get_index, name='index'),
+    path('', index, name='index'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from core.utils.endpoints import get_geo_data, search
 
 
-def get_index(request):
-    return render(request, 'core/index.html')
+def index(request):
+    """Get data for data input for customer(location_in) or automatic search(location)"""
+    
+    data = []
+
+    city = None
+
+    while not city:
+        response = get_geo_data()
+        city = response['city']
+
+    qs = request.GET.get('key', None)
+    location_in = request.GET.get('location_in', None)
+
+    location = city
+
+    context = {'city': city, 'search': False}
+
+    if location_in:
+        location = location_in
+
+    if qs:
+        data = search(keyword=qs, location=location)
+
+        context = {
+            'data': data,
+            'city': location,
+            'search': True
+        }
+
+    return render(request, 'core/index.html', context)
